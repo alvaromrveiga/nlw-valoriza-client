e4ea4e52729b1ac7ad1535d1aa0d3be4d1544f7f
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-02-21 21:14:01 -0300

    Add fontFamily

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { theme } from "./styles/theme";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider resetCSS>
+      <ChakraProvider resetCSS theme={theme}>
         <App />
       </ChakraProvider>
     </BrowserRouter>
